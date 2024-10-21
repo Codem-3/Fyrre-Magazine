2ace8de0523b66a9525ffebad41416187c6f4558
MohamedMohamed <mohamed.m.abdalfattah@gmail.com>
2024-10-21 08:01:55 +0300

    main

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primaryColor: "#FFFFFF",
+        secondaryColor: "#000000",
+      },
+      screens: {
+        '2xl': '1536px',
+        'xl': '1280px',
+        'lg': '1024px',
+        'md': '768px',
+        'sm': '640px',
+      },
+    },
+  },
+  plugins: [],
+}
