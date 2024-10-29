8aba1295c26308ac4c48583f1ca7da390f5f07e4
MohamedMohamed <mohamed.m.abdalfattah@gmail.com>
2024-10-29 21:08:22 +0300

    ADD All Details Pages

---
+++ src/pages/author/AuthorDetailsPage.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const AuthorDetailsPage = () => {
+    return (
+        <div>AuthorDetailsPage</div>
+    )
+}
---
+++ src/pages/magazine/MagazineDetailsPage.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const MagazineDetailsPage = () => {
+    return (
+        <div>MagazineDetailsPage</div>
+    )
+}
