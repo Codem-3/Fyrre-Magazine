ab80d3b41fb023729765b61d3d7e2c9be40295d4
MohamedMohamed <mohamed.m.abdalfattah@gmail.com>
2024-10-30 23:02:26 +0300

    Responsive BreakNews

--- src/components/for_landing/BreakNews.jsx
+++ src/components/for_landing/BreakNews.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 export const BreakNews = () => {
     return (
         <main className="flex items-center bg-secondaryColor text-primaryColor overflow-hidden">
-            <div className="text-nowrap font-semibold p-5 bg-primaryColor text-secondaryColor border-2 border-secondaryColor z-10">Breaking News</div>
-            <p className="flex gap-3 whitespace-nowrap animate-marquee p-5">
+            <div className="text-nowrap font-semibold px-2 py-2 sm:p-5 bg-primaryColor text-secondaryColor border-2 border-secondaryColor z-10">Breaking News</div>
+            <p className="flex gap-3 whitespace-nowrap animate-marquee">
                 <p>Electric Vehicles Expected to Dominate the Market by 2030!</p>
                 <p>E-commerce Platforms See Significant Growth During Holiday Season!</p>
                 <p>AI Diagnoses Diseases Faster Than Ever!</p>
