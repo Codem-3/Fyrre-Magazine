eb5f0891dac749c3401041ca8765036c4c70e45e
MohamedMohamed <mohamed.m.abdalfattah@gmail.com>
2024-10-31 11:50:49 +0300

    Clean Bars

--- src/components/bars/Header.jsx
+++ src/components/bars/Header.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+// Icons
 import { InstagramIcon, TwitterIcon, WifiIcon, YoutubeIcon } from '@/utils/icons.util';
 
 export const Header = (props) => {
@@ -9,29 +10,40 @@ export const Header = (props) => {
         setIsMenuOpen(!isMenuOpen);
     };
 
+    const styles = {
+        linkStyle: `transition-all hover:text-[#B17457] duration-300`,
+    }
+
     return (
         <header className="mt-6 sm:mt-12 mx-5 xl:mx-20 dark:bg-darkPrimaryColor dark:text-darkSecondaryColor">
             <main className="flex justify-between items-center mb-4">
+
                 {/* Logo */}
-                <p className="uppercase text-2xl font-bold">Fyrre Magazine</p>
+                <p className="uppercase text-2xl font-bold transition-all hover:text-[#B17457] duration-300">
+                    Fyrre Magazine
+                </p>
+
                 {/* Burger Menu Icon */}
                 <div className="block md:hidden" onClick={toggleMenu}>
                     <div className={`h-1 w-8 bg-current mb-1 transition-all ${isMenuOpen ? "rotate-45 translate-y-2" : ""}`} />
                     <div className={`h-1 w-8 bg-current mb-1 transition-all ${isMenuOpen ? "opacity-0" : ""}`} />
                     <div className={`h-1 w-8 bg-current transition-all ${isMenuOpen ? "-rotate-45 -translate-y-2" : ""}`} />
                 </div>
+
                 {/* Navigation */}
                 <ul className={`flex md:flex gap-6 items-center text-xl ${isMenuOpen ? "flex-col md:flex-row absolute md:static bg-white md:bg-transparent shadow-2xl md:shadow-none top-20 right-5 p-5 border border-secondaryColor z-50" : "hidden md:flex"}`}>
-                    <Link to={props.FirstNavLink} className="hover:underline transition-all hover:text-[#B17457] duration-300">
+
+                    <Link to={props.FirstNavLink} className={styles.linkStyle}>
                         <li>{props.FirstNav}</li>
                     </Link>
-                    <Link to={props.SecondNavLink} className="hover:underline transition-all hover:text-[#B17457] duration-300">
+                    <Link to={props.SecondNavLink} className={styles.linkStyle}>
                         <li>{props.SecondNav}</li>
                     </Link>
-                    <Link to={props.ThirdNavLink} className="hover:underline transition-all hover:text-[#B17457] duration-300">
+                    <Link to={props.ThirdNavLink} className={styles.linkStyle}>
                         <li>{props.ThirdNav}</li>
                     </Link>
                     <hr className="w-4 border-secondaryColor" />
+
                     {/* Social Icons */}
                     <div className="flex gap-3 items-center">
                         {[InstagramIcon, TwitterIcon, YoutubeIcon, WifiIcon].map((Icon, idx) => (
@@ -43,6 +55,7 @@ export const Header = (props) => {
                             </span>
                         ))}
                     </div>
+
                 </ul>
             </main>
             <hr className="border-secondaryColor" />
